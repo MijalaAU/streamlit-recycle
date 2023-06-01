da6f67b4adbaa1943d07fcab1c75d9839903b831
Michael Lamb <michael@mijala.au>
2023-06-01 11:52:57 +1000

    Removed incorrect Library References

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,8 @@
 import streamlit as st
-from rembg import remove
 from PIL import Image
-from io import BytesIO
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-import base64
 
 st.set_page_config(layout="wide", page_title="Recycle Analyser")
 
